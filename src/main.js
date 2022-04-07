1bf2e79b18af3acc97c856a736c8363b4d85fe94
Parth Gupta <parthgupta023@gmail.com>
2022-04-06 20:26:31 -0400

    Completed inference

--- src/main.js
+++ src/main.js
@@ -13,10 +13,10 @@ $(function() {
     //setup listeners 
     canvas.on('mouse:up', function(e) {
         getFrame();
-        mousePressed = false
+        mousedown = false
     });
     canvas.on('mouse:down', function(e) {
-        mousePressed = true
+        mousedown = true
     });
     canvas.on('mouse:move', function(e) {
         recordCoor(e)
@@ -24,48 +24,59 @@ $(function() {
 })
 
 
+/*
+set the table of the predictions 
+*/
+function setTable(top5, probs) {
+    //loop over the predictions 
+    for (var i = 0; i < top5.length; i++) {
+        let sym = document.getElementById('sym' + (i + 1))
+        let prob = document.getElementById('prob' + (i + 1))
+        sym.innerHTML = top5[i]
+        prob.innerHTML = Math.round(probs[i] * 100)
+    }
+
+}
+
+
 function recordCoor(event) {
     var pointer = canvas.getPointer(event.e);
     var posX = pointer.x;
     var posY = pointer.y;
 
-    if (posX >= 0 && posY >= 0 && mousePressed) {
-        coords.push(pointer)
+    if (posX >= 0 && posY >= 0 && mousedown) {
+        coordinates.push(pointer)
     }
 }
 
 
 function getMinBox() {
-    //get coordinates 
-    var coorX = coords.map(function(p) {
+    var coorX = coordinates.map(function(p) {
         return p.x
     });
-    var coorY = coords.map(function(p) {
+    var coorY = coordinates.map(function(p) {
         return p.y
     });
 
-    //find top left and bottom right corners 
-    var min_coords = {
+    var min_coordinates = {
         x: Math.min.apply(null, coorX),
         y: Math.min.apply(null, coorY)
     }
-    var max_coords = {
+    var max_coordinates = {
         x: Math.max.apply(null, coorX),
         y: Math.max.apply(null, coorY)
     }
 
     //return as strucut 
     return {
-        min: min_coords,
-        max: max_coords
+        min: min_coordinates,
+        max: max_coordinates
     }
 }
 
 function getImageData() {
-    //get the minimum bounding box around the drawing 
     const mbb = getMinBox()
 
-    //get image data according to dpi 
     const dpi = window.devicePixelRatio
     const imgData = canvas.contextContainer.getImageData(mbb.min.x * dpi, mbb.min.y * dpi,
                                                   (mbb.max.x - mbb.min.x) * dpi, (mbb.max.y - mbb.min.y) * dpi);
@@ -73,3 +84,106 @@ function getImageData() {
 }
 
 
+function getFrame() {
+    if (coordinates.length >= 2) {
+
+        const imgData = getImageData()
+
+        const pred = model.predict(preprocess(imgData)).dataSync()
+        const indices = findIndicesOfMax(pred, 5)
+        const probs = findTopValues(pred, 5)
+        const names = getclasses(indices)
+        setTable(names, probs)
+    }
+
+}
+
+function getclasses(indices) {
+    var outp = []
+    for (var i = 0; i < indices.length; i++)
+        outp[i] = classes[indices[i]]
+    return outp
+}
+
+async function loadDict() {
+    loc = 'model_tfjs/class_names.txt'
+    
+    await $.ajax({
+        url: loc,
+        dataType: 'text',
+    }).done(success);
+}
+
+function success(data) {
+    const lst = data.split(/\n/)
+    for (var i = 0; i < lst.length - 1; i++) {
+        let symbol = lst[i]
+        classes[i] = symbol
+    }
+}
+
+function findIndicesOfMax(inp, count) {
+    var outp = [];
+    for (var i = 0; i < inp.length; i++) {
+        outp.push(i); // add index to output array
+        if (outp.length > count) {
+            outp.sort(function(a, b) {
+                return inp[b] - inp[a];
+            }); // descending sort the output array
+            outp.pop(); // remove the last index (index of smallest element in output array)
+        }
+    }
+    return outp;
+}
+
+function findTopValues(inp, count) {
+    var outp = [];
+    let indices = findIndicesOfMax(inp, count)
+    // show 5 greatest scores
+    for (var i = 0; i < indices.length; i++)
+        outp[i] = inp[indices[i]]
+    return outp
+}
+
+
+
+function preprocess(imgData) {
+    return tf.tidy(() => {
+        let tensor = tf.browser.fromPixels(imgData, numChannels = 1)
+        
+        const resized = tf.image.resizeBilinear(tensor, [28, 28]).toFloat()
+        
+        const offset = tf.scalar(255.0);
+        const normalized = tf.scalar(1.0).sub(resized.div(offset));
+
+        const batched = normalized.expandDims(0)
+        return batched
+    })
+}
+
+
+
+async function start() {
+    model = await tf.loadLayersModel('model_tfjs/model.json') 
+    model.predict(tf.zeros([1, 28, 28, 1]))
+    allowDrawing()
+    await loadDict()
+}
+
+
+function allowDrawing() {
+    canvas.isDrawingMode = 1;
+    document.getElementById('status').innerHTML = 'Model Loaded';
+    $('button').prop('disabled', false);
+    var slider = document.getElementById('myRange');
+    slider.oninput = function() {
+        canvas.freeDrawingBrush.width = this.value;
+    };
+}
+
+
+function erase() {
+    canvas.clear();
+    canvas.backgroundColor = '#ffffff';
+    coordinates = [];
+}
